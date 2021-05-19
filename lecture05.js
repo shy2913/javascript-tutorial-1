eacc160fa71ac0780b2bcebbc248d71c0fc725e6
shy2913 <66987722+shy2913@users.noreply.github.com>
2021-05-19 21:41:07 +0900

    javascript practice 2

---
+++ lecture05.js
@@ -0,0 +1,8 @@
+var height=prompt("키를 입력해주세요!");
+console.log(height,typeof(height));
+ 
+var height_int=parseInt(height); //입력받은 키 값을 정수로 변환!
+console.log(height_int,typeof(height_int)); //height_int값과 데이터 타입 출력!
+var height_float=parseFloat(height);
+console.log(height_float,typeof(height_float));
+

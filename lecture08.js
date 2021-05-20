b1404fba6713c061ba052a1c842f64b2f6ee77ea
shy2913 <66987722+shy2913@users.noreply.github.com>
2021-05-20 21:14:01 +0900

    javascript practice

---
+++ lecture08.js
@@ -0,0 +1,16 @@
+function retunrn_test() {
+	return;
+	console.log("실행되지 않는 코드");
+}
+
+function print(msg) {
+	console.log("print function in");
+	console.log(msg);
+	console.log("print function out")
+}
+
+function sum(arg1,arg2) {
+	var result=arg1+arg2;
+	return result;
+}
+
